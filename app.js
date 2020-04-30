9e6a4db9e7bdc92bf6d0a29b42301259114a09dc
Dimitar Chukovski <59423614+chukovskid@users.noreply.github.com>
2020-04-30 12:30:02 +0200

    so bezbednost

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express     = require("express"),
     passport    = require("passport"),
     cookieParser = require("cookie-parser"),
     LocalStrategy = require("passport-local"),
+    nodemailer = require('nodemailer'),
     flash        = require("connect-flash"),
     Campground  = require("./models/campground"),
     Comment     = require("./models/comment"),
@@ -81,6 +82,64 @@ app.use(function(req, res, next){
 app.use("/", indexRoutes);
 app.use("/mai4ki", campgroundRoutes);
 app.use("/mai4ki/:id/comments", commentRoutes);
+
+
+
+app.get('/send', (req, res)=>{
+    res.render('contact');
+})
+
+// Email
+app.post('/send', (req, res) => {
+    const output = `
+      <p>You have a new contact request</p>
+      <h3>Contact Details</h3>
+      <ul>  
+        <li>Name: ${req.body.name}</li>
+        <li>Company: ${req.body.company}</li>
+        <li>Email: ${req.body.email}</li>
+        <li>Phone: ${req.body.phone}</li>
+      </ul>
+      <h3>Message</h3>
+      <p>${req.body.message}</p>
+    `;
+  
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: 'mail.ristomanova.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+          user: 'maicka@ristomanova.com', // generated ethereal user
+          pass: 'maicka1234'  // generated ethereal password
+      },
+      tls:{
+        rejectUnauthorized:false
+      }
+    });
+  
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Mai4ka" <maicka@ristomanova.com', // sender address
+        to: 'c.dimkooo@gmail.com', // list of receivers
+        subject: 'Node Contact Request', // Subject line
+        text: 'Hello world?', // plain text body
+        html: output // html body
+    };
+  
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);   
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  
+        res.render('contact', {msg:'Email has been sent'});
+    });
+    });
+// Email End
+
 
 app.listen(3000 || process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
